5f38f0063d9bb96f89583ff1be50d9e18494e506
Max <chernyakovich96@mail.ru>
2020-04-08 19:43:36 +0300

    add script file, create function caseList

---
+++ src/js/script.js
@@ -0,0 +1,39 @@
+
+"use strict";
+
+
+
+// variables
+const input = document.querySelector('.enter__input'),
+      ul = document.querySelector('.list__case'),
+      btnAdd = document.querySelector('.enter__btn'),
+      btnSave = document.querySelector('.btn__save'),
+      btnClear = document.querySelector('.btn__clear');
+
+     // create case 
+     function caseList() {
+          
+          // create list item
+          let li = document.createElement('li');
+          li.classList.add('list__case--item');
+
+          // crate text in list item
+          let caseText = document.createElement('p');
+          caseText.classList.add('case__text');
+
+          // create input value
+          let newInput = input.value;
+          caseText.append(newInput);
+
+          ul.appendChild(li).append(caseText);
+          
+          // if input = nothing
+          if (input.value == '') {
+               return alert('complete add');
+          } else {
+               return input.value
+          };
+     };
+
+     // click event for btn
+     btnAdd.addEventListener('click', caseList);
